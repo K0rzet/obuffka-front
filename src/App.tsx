e6e56cd7ff350ea4310415310f0300211e43b553
Илья Буторин <ilyabutorin@MacBook-Pro-Ila.local>
2025-01-29 00:34:08 +0300

    improved: design

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { observer } from 'mobx-react-lite';
+import { RiHome2Line, RiSettings4Line } from 'react-icons/ri';
 import HomePage from './pages/HomePage/HomePage';
 import AdminPage from './pages/AdminPage/AdminPage';
 import { authStore } from './store/AuthStore';
@@ -15,9 +16,13 @@ const App: React.FC = observer(() => {
         <BrowserRouter>
             <div className={styles.app}>
                 <nav className={styles.nav}>
-                    <Link to="/">Главная</Link>
+                    <Link to="/" title="Главная">
+                        <RiHome2Line size={24} />
+                    </Link>
                     {authStore.user?.isAdmin && (
-                        <Link to="/admin">Админ панель</Link>
+                        <Link to="/admin" title="Админ панель">
+                            <RiSettings4Line size={24} />
+                        </Link>
                     )}
                 </nav>
                 
